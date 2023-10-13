641763d864655efda459a0e284861328b59df53e
Hanieh1995 <hony1374@gmail.com>
2023-10-13 13:30:56 +0330

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,14 @@ function reducer(state, { type, payload }) {
         }
       }
       if (payload.digit === "0" && state.currentOperand === "0") return state;
+      if (payload.digit === "." && !state.currentOperand) {
+        return {
+          ...state,
+          currentOperand: "0" + payload.digit
+        }
+      }
       if (payload.digit === "." && state.currentOperand.includes(".")) return state;
+
       return {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
@@ -51,23 +58,23 @@ function reducer(state, { type, payload }) {
       }
     case ACTIONS.CLEAR:
       return {}
-      case ACTIONS.DELETE_DIGIT:
-        if(state.overwrite){
-          return {
-            ...state,
-            overwrite: false,
-            currentOperand: null,
-          }
-        } 
-        if(state.currentOperand == null) return state
-        if(state.currentOperand.length === 1){
-          return {...state, currentOperand:null}
-        }
-
-        return{
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return {
           ...state,
-          currentOperand: state.currentOperand.slice(0,-1)
+          overwrite: false,
+          currentOperand: null,
         }
+      }
+      if (state.currentOperand == null) return state
+      if (state.currentOperand.length === 1) {
+        return { ...state, currentOperand: null }
+      }
+
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1)
+      }
 
     case ACTIONS.EVALUATE_DIGIT:
       if (state.operation == null || state.currentOperand == null || state.previousOperand == null) return state
@@ -110,9 +117,9 @@ const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
 })
 
 function formatOperand(operand) {
-  if(operand == null) return
+  if (operand == null) return
   const [integer, decimal] = operand.split('.')
-  if(decimal == null) return INTEGER_FORMATTER.format(integer)
+  if (decimal == null) return INTEGER_FORMATTER.format(integer)
   return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
 }
 
@@ -125,7 +132,7 @@ function App() {
         <div className="current-operand">{formatOperand(currentOperand)}</div>
       </div>
       <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
-      <button onClick={() => dispatch({type: ACTIONS.DELETE_DIGIT})}>DEL</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>DEL</button>
       <OperationButton operation="/" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
